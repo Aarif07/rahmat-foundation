b1bbcc96c791af1eb3dadfe40780ee795190ce1d
Aarif07 <70398661+Aarif07@users.noreply.github.com>
2026-05-27 15:44:44 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,61 @@
+
+const CACHE_NAME = 'rahmat-foundation-offline-v7';
+
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/admin.html',
+  '/manifest.json',
+  '/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+
+      })
+      .catch(() => {
+
+        return caches.match(event.request)
+          .then(cachedResponse => {
+            return cachedResponse || caches.match('/index.html');
+          });
+
+      })
+  );
+});
